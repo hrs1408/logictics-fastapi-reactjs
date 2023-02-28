2a8f92cb8599f6a9da7b69a93143a2e9c0285653
nguyenvanhoang46 <nguyenvanhoang4601@gmail.com>
2023-02-28 22:35:21 +0700

    delivery

--- client/src/components/ListDelivery/index.tsx
+++ client/src/components/ListDelivery/index.tsx
@@ -6,7 +6,7 @@ const ListDelivery = () => {
     <div className="container mx-auto">
       <div className="grid grid-cols-8">
         <Link to={"/"} className="item-service text-center">
-          <p className="mb-3 h-[50px] flex justify-center">
+          <p className=" h-[50px] flex justify-center">
             <img
               className="w-[55px]"
               src="https://ntlogistics.vn/images/icon_hoa_toc.png"
@@ -15,67 +15,67 @@ const ListDelivery = () => {
           Chuyển phát Hỏa tốc
         </Link>
         <Link to={"/"} className="item-service text-center">
-          <p className="mb-3 h-[50px] flex justify-center">
+          <p className=" h-[50px] flex justify-center">
             <img
               className="w-[55px]"
               src="https://ntlogistics.vn/images/icon_nhanh.png"
             />
           </p>
-          Chuyển phát Hỏa tốc
+          Chuyển phát Nhanh
         </Link>
         <Link to={"/"} className="item-service text-center">
-          <p className="mb-3 h-[50px] flex justify-center">
+          <p className=" h-[50px] flex justify-center">
             <img
               className="w-[55px]"
               src="https://ntlogistics.vn/images/icon_tiet_kiem.png"
             />
           </p>
-          Chuyển phát Hỏa tốc
+          Chuyển phát Tiết kiệm (MES)
         </Link>
         <Link to={"/"} className="item-service text-center">
-          <p className="mb-3 h-[50px] flex justify-center">
+          <p className=" h-[50px] flex justify-center">
             <img
               className="w-[55px]"
               src="https://ntlogistics.vn/images/icon_duong_bo.png"
             />
           </p>
-          Chuyển phát Hỏa tốc
+          Chuyển phát Đường bộ
         </Link>
         <Link to={"/"} className="item-service text-center">
-          <p className="mb-3 h-[50px] flex justify-center">
+          <p className=" h-[50px] flex justify-center">
             <img
               className="w-[55px]"
               src="https://ntlogistics.vn/images/icon_thu_ho.png"
             />
           </p>
-          Chuyển phát Hỏa tốc
+          Chuyển phát Thu hộ (COD)
         </Link>
         <Link to={"/"} className="item-service text-center">
-          <p className="mb-3 h-[50px] flex justify-center">
+          <p className=" h-[50px] flex justify-center">
             <img
               className="w-[55px]"
               src="https://ntlogistics.vn/images/icon_quoc_te.png"
             />
           </p>
-          Chuyển phát Hỏa tốc
+          Chuyển phát Quốc tế
         </Link>
         <Link to={"/"} className="item-service text-center">
-          <p className="mb-3 h-[50px] flex justify-center">
+          <p className=" h-[50px] flex justify-center">
             <img
               className="w-[55px]"
               src="https://ntlogistics.vn/images/icon_nguyen_chuyen.png"
             />
           </p>
-          Chuyển phát Hỏa tốc
+          Thuê xe Nguyên chuyến
         </Link>
         <Link to={"/"} className="item-service text-center">
-          <p className="mb-3 h-[50px] flex justify-center">
+          <p className=" h-[50px] flex justify-center">
             <img
               className="w-[55px]"
               src="https://ntlogistics.vn/images/icon_3pl.png"
             />
           </p>
-          Chuyển phát Hỏa tốc
+          3PL & Fulfillment
         </Link>
       </div>
     </div>
