29f6e76b04410fb43d4679623f80ffa37345fee7
nguyenvanhoang46 <93237316+nguyenvanhoang46@users.noreply.github.com>
2023-03-03 15:13:01 +0700

    fix: env api
    fix: env api

---
+++ client/src/components/Iframe/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./index.scss"
+
+
+const Iframe = () => {
+    return (
+        <>
+            <div className='w-full flex justify-center mt-20'>
+                <iframe
+                    width="1120"
+                    height="630"
+                    src="https://www.youtube.com/embed/WEC92P-B-Rs" >
+                </iframe>
+            </div>
+            <div className="container mx-auto ">
+                <div className="wp-kh w-full text-center">
+                    <h2 className='text-kh inline-block pt-5 pb-0 text-[24px]'>
+                        <div className="mt-10">
+                            <b className="">Khách hàng</b>
+                            của Nhất Tín Logistics
+                        </div>
+                    </h2>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Iframe
