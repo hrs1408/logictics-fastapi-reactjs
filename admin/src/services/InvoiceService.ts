f1d13c786ac7c7943335c78ba90b53401506cab2
NTAK666 <=>
2023-03-11 16:41:28 +0700

    * 🐛 fix(AuthService.ts): remove expiration time from Cookies.set calls * ✨ feat(InvoiceService.ts): add functions to get all invoices, update invoice status, and delete invoice The expiration time was removed from the Cookies.set calls in AuthService.ts as it was causing issues with the cookies not being set correctly. In InvoiceService.ts, functions were added to get all invoices, update invoice status, and delete invoice. These functions are used to fetch and manipulate invoice data in the application.
    * ✨ feat(OtherService.ts): change query key from 'GET_PROVINCE' to 'GET_PROVINCES'
    * ✨ feat(PortService.ts): add CRUD operations for ports and their respective react-query hooks
    The query key for the useProvince hook has been changed from 'GET_PROVINCE' to 'GET_PROVINCES' to better reflect the data being fetched. The PortService now has CRUD operations for ports, including useGetAllPorts, useGetPortById, useCreatePort, useUpdatePort, and useDeletePort react-query hooks. These hooks allow for easy management of port data in the application.
    
    * ✨ feat(UserService.ts): add updateUser function and useUpdateUser hook
    * ✨ feat(UserService.ts): add getUserById function and useUserById hook
    * ♻️ refactor(UserService.ts): modify getUsers function to accept query parameters
    The getUsers function now accepts query parameters such as page, size, search, and isFull. This allows for more flexibility in fetching users. The updateUser function and useUpdateUser hook are added to allow updating user information. The getUserById function and useUserById hook are added to fetch a single user by their ID.

--- admin/src/services/InvoiceService.ts
+++ admin/src/services/InvoiceService.ts
@@ -1,12 +1,57 @@
 import axiosConfig from '../configs/AxiosConfig'
-import { useMutation } from 'react-query'
+import { useMutation, useQuery } from 'react-query'
 
 export const CreateInvoice = (
   data: CreateInvoiceType
-): Promise<ResponseSuccessType<InvoiceCreateResponse>> => {
+): Promise<ResponseSuccessType<InvoiceType>> => {
   return axiosConfig.post(`/invoice`, data)
 }
 
 export const useCreateInvoice = () => {
   return useMutation(['CREATE_INVOICE'], CreateInvoice)
+}
+
+export const getInvoices = ({
+  page,
+  size,
+  search,
+  isFull,
+}: QueryParamType): Promise<ResponseSuccessType<InvoiceType[]>> => {
+  return axiosConfig.get(`/invoice`, {
+    params: {
+      page,
+      size,
+      search,
+      isFull,
+    },
+  })
+}
+
+export const useGetAllInvoices = ({
+  page = 1,
+  size = 10,
+  search = '',
+  isFull = true,
+}: QueryParamType) => {
+  return useQuery(
+    ['GET_INVOICES', page, size, search, isFull],
+    () => getInvoices({ page, size, search, isFull }),
+    {
+      refetchOnMount: true,
+      refetchOnWindowFocus: true,
+    }
+  )
+}
+
+const updateStatusInvoices = (data: InvoicesUpdateStatusPayload) =>
+  axiosConfig.put(`invoice/change-status`, data)
+
+export const useUpdateStatusInvoices = () => {
+  return useMutation(['UPDATE_STATUS_INVOICES'], updateStatusInvoices)
+}
+
+const deleteInvoice = (id: string) => axiosConfig.delete(`invoice/${id}`)
+
+export const useDeleteInvoice = () => {
+  return useMutation(['DELETE_INVOICE'], deleteInvoice)
 }
--- admin/src/services/UserService.ts
+++ admin/src/services/UserService.ts
@@ -1,16 +1,46 @@
 import axiosConfig from '../configs/AxiosConfig'
 import { useMutation, useQuery } from 'react-query'
 
-export const getUsers = (): Promise<ResponseSuccessType<UserType[]>> =>
-  axiosConfig.get(`/users`)
-
-export const useUsers = () => {
-  return useQuery(['GET_USERS'], getUsers)
+export const getUsers = ({
+  search = '',
+}: QueryParamType): Promise<ResponseSuccessType<UserType[]>> => {
+  return axiosConfig.get(`/users`, {
+    params: {
+      page: 1,
+      size: 100,
+      search,
+      isFull: true,
+    },
+  })
+}
+export const useUsers = ({ page, size, search, isFull }: QueryParamType) => {
+  return useQuery(['GET_USERS', search], () =>
+    getUsers({ page, size, search, isFull })
+  )
 }
 
-export const createUser = (data: any): Promise<ResponseSuccessType<UserType>> =>
+export const createUser = (
+  data: CreateUserType
+): Promise<ResponseSuccessType<UserType>> =>
   axiosConfig.post(`/users/add-user-internal`, data)
 
 export const useCreateUser = () => {
   return useMutation(['CREATE_USER'], createUser)
+}
+export const updateUser = (
+  data: UpdateUserType
+): Promise<ResponseSuccessType<UserType>> =>
+  axiosConfig.put(`/users/information`, data)
+
+export const useUpdateUser = () => {
+  return useMutation(['UPDATE_USER'], updateUser)
+}
+
+const getUserById = (id: number): Promise<ResponseSuccessType<UserType>> =>
+  axiosConfig.get(`/users/${id}`)
+
+export const useUserById = (id: number) => {
+  return useQuery(['GET_USER_BY_ID', id], () => getUserById(id), {
+    enabled: !!id,
+  })
 }
